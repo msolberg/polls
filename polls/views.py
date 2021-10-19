f7f42fce1963d041b13b0335dcc58fe4cf9fc275
Michael Solberg <msolberg@redhat.com>
2021-10-19 14:27:36 -0400

    Adding a new title

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     """Home page"""
-    return HttpResponse("Polls application.")
+    return HttpResponse("Polls application. New Title!")
 
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
