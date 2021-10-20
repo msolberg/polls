8f636515fb39f658bc58c3539806d438be6ad61b
Michael Solberg <msolberg@redhat.com>
2021-10-20 13:20:42 -0400

    Updating views and urls

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    """Home page"""
-    return HttpResponse("Polls application. New Title!")
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
--- project/urls.py
+++ project/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
 
     url(r'^$', index),
     url(r'^health$', health),
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
